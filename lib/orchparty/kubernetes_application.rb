73e9fa3d2184e17223e19ff346b6534f414a3436
Peter Schrammel <peter.schrammel@gmx.de>
2022-11-26 19:46:51 +0000

    add chart to output

--- lib/orchparty/kubernetes_application.rb
+++ lib/orchparty/kubernetes_application.rb
@@ -243,8 +243,9 @@ module Orchparty
       def print_install(chart)
         
         build_chart(chart) do |chart_path|
-          cmd= "helm template --namespace #{namespace} --kube-context #{cluster_name} #{chart.name} #{chart_path} > /dev/null"
-          if system(cmd)
+          cmd="helm template --namespace #{namespace} --kube-context #{cluster_name} #{chart.name} #{chart_path}"
+          @out_io.puts `$cmd`
+          if system("#{cmd} > /dev/null")
             info("helm template check: OK")
           else
             error("helm template check: FAIL")
