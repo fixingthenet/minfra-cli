bc0d293004d2acafb9c4785ebe552e04540280a3
Peter Schrammel <peter.schrammel@conceptboard.com>
2023-01-03 22:12:54 +0000

    adding minfra_path to scope

--- lib/minfra/cli.rb
+++ lib/minfra/cli.rb
@@ -79,7 +79,7 @@ module Minfra
       hiera_main_path=@hiera_root.join("hieradata/#{config.project.minfra.hiera.env_path}/#{env}.eyaml")
       raise("unknown environment #{env}, I expact a file at #{hiera_main_path}") unless hiera_main_path.exist? 
 
-      scope={ "hieraroot" => @hiera_root.to_s, "env" => env}
+      scope={ "minfra_path" => ENV["MINFRA_PATH"], "hieraroot" => @hiera_root.to_s, "env" => env}
       special_lookups=hiera.lookup("lookup_options", {},  scope, nil, :priority)
       
       node_scope=hiera.lookup("env", {},  scope, nil, :deeper)
