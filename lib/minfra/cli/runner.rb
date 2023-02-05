34f973ec9cf2fd2ca9da15a240a6538c5619935c
Peter Schrammel <peter.schrammel@gmx.de>
2023-02-05 21:12:00 +0000

    runner now prints output at runtime

--- lib/minfra/cli/runner.rb
+++ lib/minfra/cli/runner.rb
@@ -4,13 +4,27 @@ module Minfra
   module Cli
     class Runner
       class Result
-        attr_reader :stdout, :stderr
-        def initialize(stdout,stderr, status)
-          @stderr=stderr
-          @stdout=stdout
-          @status=status
+        include Logging
+        
+        attr_writer :status
+        
+        def initialize
+          @stderr_lines=[]
+          @stdout_lines=[]
+          @status=nil
         end
-
+        
+        def add(line, stream=:stdout)
+          line.chomp!
+          if stream == :stdout
+            @stdout_lines << line
+            debug line
+          else
+            @stderr_lines << line
+            error line
+          end
+        end
+           
         def success?
           @status.success?
         end
@@ -19,8 +33,16 @@ module Minfra
           !success?
         end
 
+        def stdout
+          @stdout_lines.join("\n")
+        end
+        
+        def stderr
+          @stderr_lines.join("\n")
+        end  
+        
         def to_s
-          @stdout.to_s
+          stdout
         end
       end
 
@@ -36,24 +58,37 @@ module Minfra
       end
 
       def run
-        debug(@cmd)
+        debug("running: #{@cmd}")
         res=nil
         begin
-          res=Result.new(*Open3.capture3(@cmd))
+          res=Result.new
+          # see: http://stackoverflow.com/a/1162850/83386
+          Open3.popen3(@cmd) do |stdin, stdout, stderr, thread|
+            # read each stream from a new thread
+            { :stdout => stdout, :stderr => stderr }.each do |key, stream|
+              Thread.new do
+                until (raw_line = stream.gets).nil? do
+                  res.add(raw_line, key)
+                end
+              end
+            end
+            thread.join # don't exit until the external process is done
+            res.status = thread.value
+          end
         rescue
-        end  
-        if res&.error?
-          STDERR.puts "command failed: #{@cmd}"
-          STDERR.puts res.stdout
-          STDERR.puts res.stderr
         end
-        if exit_on_error && res&.error?
-          STDERR.puts "exiting on error"
+        
+        if res.error?
+          error "command failed: #{@cmd}"
+          info  res.stdout
+          error res.stderr
+        end
+        if exit_on_error && res.error?
+          info "exiting on error"
           exit 1
         end
         res
       end
-
     end
   end
 end
