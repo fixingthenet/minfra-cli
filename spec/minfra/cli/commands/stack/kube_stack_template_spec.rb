e3a6806ff25904ac6648c97378cad1b95c662029
Peter Schrammel <peter.schrammel@gmx.de>
2023-02-06 00:29:45 +0000

    some Gemfile.lock updates, getting rspec working again

--- spec/minfra/cli/commands/stack/kube_stack_template_spec.rb
+++ spec/minfra/cli/commands/stack/kube_stack_template_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 require 'minfra/cli'
+require 'minfra/cli/commands/stack/kube_stack_template.rb'
 
 RSpec.describe Minfra::Cli::StackM::KubeStackTemplate do
   it "should init" do
