5f4e3589317d8ae68673832dd80bc3cdcf750a35
Peter Schrammel <peter.schrammel@gmx.de>
2023-12-20 23:09:01 +0000

    no fake config

--- spec/minfra/cli/commands/stack/kube_stack_template_spec.rb
+++ spec/minfra/cli/commands/stack/kube_stack_template_spec.rb
@@ -6,8 +6,7 @@ require 'minfra/cli/commands/stack/kube_stack_template.rb'
 RSpec.describe Minfra::Cli::StackM::KubeStackTemplate do
   let!(:main) { Minfra::Cli.init}
   it "should init" do
-     config = OpenStruct.new(stacks_path: Pathname.new('/tmp'), status_path: Pathname.new('/tmp/status'))
-     Minfra::Cli::StackM::KubeStackTemplate.new("name", config, deployment: nil, cluster: '')
+     Minfra::Cli::StackM::KubeStackTemplate.new("name", Minfra::Cli.config, deployment: nil, cluster: '')
   end
 end
 
