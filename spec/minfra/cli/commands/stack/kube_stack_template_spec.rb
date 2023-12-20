902e1055e2833e4738ed05543db4a6d8b045a953
Peter Schrammel <peter.schrammel@gmx.de>
2023-12-20 23:11:55 +0000

    no init needed

--- spec/minfra/cli/commands/stack/kube_stack_template_spec.rb
+++ spec/minfra/cli/commands/stack/kube_stack_template_spec.rb
@@ -4,7 +4,6 @@ require 'minfra/cli'
 require 'minfra/cli/commands/stack/kube_stack_template.rb'
 
 RSpec.describe Minfra::Cli::StackM::KubeStackTemplate do
-  let!(:main) { Minfra::Cli.init}
   it "should init" do
      Minfra::Cli::StackM::KubeStackTemplate.new("name", Minfra::Cli.config, deployment: nil, cluster: '')
   end
