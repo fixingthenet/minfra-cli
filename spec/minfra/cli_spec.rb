3c908ff18b383ef1a3ba58337c417af09f0679a6
Peter Schrammel <peter.schrammel@gmx.de>
2023-12-23 23:11:02 +0000

    don't depend on MINFRA_PATH

--- spec/minfra/cli_spec.rb
+++ spec/minfra/cli_spec.rb
@@ -10,8 +10,11 @@ describe Minfra::Cli do
   end
 
   describe 'CliStarter' do
-    let(:minfra_path) { File.expand_path(File.join(__FILE__, "../../fixture/minfra_example"))  }
-    let!(:main) { Minfra::Cli.init(['one', 'two', 'three', '-e', 'test', '--minfra_argv_file', 'hallo.txt', '--minfra_path', minfra_path]) }
+    let(:minfra_path) { File.expand_path(File.join(__FILE__, '../../fixture/minfra_example')) }
+    let!(:main) do
+      Minfra::Cli.init(['one', 'two', 'three', '-e', 'test', '--minfra_argv_file', 'hallo.txt', '--minfra_path',
+                        minfra_path])
+    end
 
     it 'extracts global_options' do
       expect(main.options).to eq({ env: 'test', argv_file: 'hallo.txt', base_path: minfra_path })
@@ -21,19 +24,19 @@ describe Minfra::Cli do
     it 'sets env' do
       expect(main.env).to eq('test')
     end
-    
-    it "sets up config correctly" do
+
+    it 'sets up config correctly' do
       expect(main.config.orch_env).to eq('test')
     end
-    
+
     it 'registers some subcommands' do
       expect(Minfra::Cli.subcommands).not_to be_empty
     end
 
     it 'runs a single command' do
-      expect(Minfra::Cli.init(['version']).run).to eq(0)
+      expect(Minfra::Cli.init(['version', '--minfra_path', minfra_path]).run).to eq(0)
     end
-    
+
     it 'runs commands from a file'
   end
 end
