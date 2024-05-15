1107147d0628650069a9e4336c3eb9c0ee7bf91f
Peter Schrammel <peter.schrammel@gmx.de>
2024-05-15 04:51:42 +0000

    fixing spec

--- spec/minfra/cli/cli_starter_spec.rb
+++ spec/minfra/cli/cli_starter_spec.rb
@@ -1,8 +1,11 @@
 require 'spec_helper'
 
 RSpec.describe Minfra::Cli::Config do
+  let(:minfra_path) {
+    File.expand_path(File.join(__FILE__, "../../../fixture/minfra_example"))
+  }
   let(:starter) {
-    Minfra::Cli::CliStarter.new(['--no-rc'])
+    Minfra::Cli::CliStarter.new(['--no-rc', '--minfra_path', minfra_path ])
   }
 
   it "parses --no-rc option" do
